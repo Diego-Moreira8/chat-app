859ea8fa2c94d05379c8cda8fc7f8b7918bb6127
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2026-05-22 09:20:56 -0300

    feat: in the database, add created at on user column

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -4,13 +4,12 @@ import express from "express";
 export const usersRouter = express.Router();
 
 usersRouter.get("/", async (req, res, next) => {
-  const userTest: User = {
-    id: 1,
-    username: "user",
-    passwordHash: "123",
-  };
-
-  const newUser = await prisma.user.create({ data: userTest });
+  const newUser = await prisma.user.create({
+    data: {
+      username: "test",
+      passwordHash: "123",
+    },
+  });
 
   res.json({ user: newUser });
 });
